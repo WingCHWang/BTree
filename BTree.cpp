88a47a2f937994bbf8cc5bea588e69c6562136dd
Chong Wang <chong.wang02@sap.com>
2017-09-04 19:26:34 +0800

    commit

--- BTree.cpp
+++ BTree.cpp
@@ -7,9 +7,9 @@ using namespace std;
 
 typedef struct Node{
     int n;
-    int key[t-1];
+    int key[2*t-1];
     bool leaf;
-    struct Node *child[t];
+    struct Node *child[2*t];
     string data;
 }Node;
 
@@ -21,6 +21,24 @@ typedef struct {
     Node* node;
     int index;
 }Pos;
+
+Node *alloc_node();
+void free_node(Node *node);
+Pos search(Node* node, int key);
+void create(Tree *tree);
+void splitChild(Node* x, int i);
+void insertNonFull(Node* node, int key);
+void insert(Tree* T, int key);
+void gothrough(Node *node, int i);
+
+
+Node *alloc_node() {
+    return new Node();
+}
+
+void free_node(Node *node) {
+    delete node;
+}
 
 Pos search(Node* node, int key) {
     int i = 0;
@@ -40,30 +58,31 @@ Pos search(Node* node, int key) {
 }
 
 void create(Tree *tree) {
-    Node root;
-    root.leaf = true;
-    root.n = 0;
-    tree->root = &root;
+    Node *root = alloc_node();
+    root->leaf = true;
+    root->n = 0;
+    tree->root = root;
     cout << "create\n";
+    
 }
 
 void splitChild(Node* x, int i) {
-    Node z;
+    Node *z = alloc_node();
     Node *y = x->child[i];
-    z.leaf = y->leaf;
-    z.n = t-1;
+    z->leaf = y->leaf;
+    z->n = t-1;
     for(int j =0; j<t-1; j++) {
-        z.key[j] = y->key[t+j];
+        z->key[j] = y->key[t+j];
     }
     if(!y->leaf) {
         for(int j = 0; j<t-1; j++)
-            z.child[j] = y->child[t+j];
+            z->child[j] = y->child[t+j];
     }
     y->n = t-1;
     for(int j = x->n+1; j>=i+1; j--) {
         x->child[j+1] = x->child[j];
     }
-    x->child[i+1] = &z;
+    x->child[i+1] = z;
     for(int j = x->n; j>=i; j--) {
         x->key[j+1] = x->key[j];
     }
@@ -72,8 +91,6 @@ void splitChild(Node* x, int i) {
 }
 
 void insertNonFull(Node* node, int key) {
-    cout << "insertNonFull"<<key<<"\n";
-    cout << node->n << "\n";
     int i = node->n-1;
     if(node->leaf) {
         while(i>=0 && key<node->key[i]) {
@@ -81,7 +98,6 @@ void insertNonFull(Node* node, int key) {
             i--;
         }
         node->key[i+1] = key;
-        cout << node->key[i+1] << "\n";
         node->n ++;
     }
     else {
@@ -101,14 +117,13 @@ void insertNonFull(Node* node, int key) {
 void insert(Tree* T, int key) {
     Node *root = T->root;
     if(root->n == 2*t-1) {
-        Node newRoot;
-        newRoot.leaf = false;
-        newRoot.n = 0;
-        newRoot.child[0] = root;
-        T->root = &newRoot;
-        cout << "insert\n";
-        splitChild(&newRoot, 0);
-        insertNonFull(&newRoot, key);
+        Node *newRoot = alloc_node();
+        newRoot->leaf = false;
+        newRoot->n = 0;
+        newRoot->child[0] = root;
+        T->root = newRoot;
+        splitChild(newRoot, 0);
+        insertNonFull(newRoot, key);
     }
     else {
         
@@ -119,8 +134,7 @@ void insert(Tree* T, int key) {
 }
 
 void gothrough(Node *node, int i) {
-    cout << i << "\n";
-    //cout << node->n << "\n";
+    cout << i << "th\n";
     for(int j = 0; j<node->n; j++) {
         cout << node->key[j] << "|";
     }
@@ -135,11 +149,9 @@ void gothrough(Node *node, int i) {
 int main(int argc, char** args) {
     Tree *tree = new Tree();
     create(tree);
-    for(int i = 0; i<10; i++) {
-        cout << tree->root->n << endl;
-        if(i == 9) 
-            gothrough(tree->root, 0);
-        insert(tree, i);
-        
+    for(int i = 0; i<50; i++) {
+        cout << i << "\n";
+        insert(tree, i);  
     }
+    gothrough(tree->root, 0);
 }
